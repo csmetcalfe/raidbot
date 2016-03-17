4e29f90896eebe93380b92cc0115a2d14efd7609
csmetcalfe <chris.s.metcalfe@gmail.com>
2016-03-17 14:46:23 -0700

    Add botkit node module

--- bot.js
+++ bot.js
@@ -1,4 +1,5 @@
 var Botkit = require('botkit');
+var http = require('http');
 
 var controller = Botkit.slackbot({
   debug: false
@@ -6,7 +7,7 @@ var controller = Botkit.slackbot({
 
 // connect the bot to a stream of messages
 controller.spawn({
-  token: 'xoxb-26777377574-7NzzqsSaWAQeI6Y0qVzBG368'
+  token: process.env.token
 }).startRTM();
 
 // give the bot something to listen for.
@@ -16,18 +17,31 @@ controller.hears('hello','direct_message,direct_mention,mention',function(bot,me
 
 controller.hears(['draw', 'pick', 'next', 'select', 'choose'],'direct_message,direct_mention,mention',function(bot,message) {
 
-  engineers = ["abaker", "cmetcalfe", "cnguyen", "jbecker", "johnny", "mthompson"];
   bugEmojis = [":ant:", ":beetle:", ":bug:", ":spider:"];
-
   var bugEmoji = bugEmojis[Math.floor(Math.random() * bugEmojis.length)];
-  var onCallEngineer = engineers[Math.floor(Math.random() * engineers.length)];
-  var onCallEngineerTag = "<@" + onCallEngineer + ">";
 
-  bot.reply(message, "Let's put " + onCallEngineerTag + " on bug duty today " + bugEmoji + " :hammer:");
+  bot.reply(message, "<@" + selectRandomEngineer() + "> is on bug duty today " + bugEmoji + " :hammer:");
 });
 
+function selectRandomEngineer() {
+  engineers = ["abaker", "cmetcalfe", "cnguyen", "jbecker", "johnny", "mthompson"];
+  return engineers[Math.floor(Math.random() * engineers.length)];
+}
 
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-}).listen(process.env.PORT || 5000);
+
+//Lets define a port we want to listen to
+const PORT = process.env.PORT || 8080;
+
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
+
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
